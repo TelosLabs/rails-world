d44d205baae8aaccca4490f3862c9959e530de1a
Sergio-e <33036058+Sergio-e@users.noreply.github.com>
2024-07-05 15:58:15 -0600

    Add missing config files and autocorrect JS files

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,13 +10,13 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/container-queries')
   ]
 }
