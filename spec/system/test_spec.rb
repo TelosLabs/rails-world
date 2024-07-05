4d6e2953c599bffcd713753a583a5f7bd45ecf4e
LuigiR0jas <luigirojasdev@gmail.com>
2024-07-05 13:45:26 -0600

    More auto corrections

--- spec/system/test_spec.rb
+++ spec/system/test_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "System specs health check", type: :system do
   it "works" do
     visit "/up"
-    expect(page).to have_http_status(200)
+    expect(page).to have_http_status(:ok)
   end
 end
