2025e60c8572111c1e8584009f02efd7444d62f7
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-07-17 14:18:23 -0600

    standard js linter

--- app/javascript/controllers/image_previewer_controller.js
+++ app/javascript/controllers/image_previewer_controller.js
@@ -2,10 +2,9 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = ['image', 'input', 'hideOnLoad']
-  
+
   connect () {
     this.inputTarget.onchange = evt => {
-      
       const [file] = this.inputTarget.files
       if (file) {
         this.imageTarget.src = URL.createObjectURL(file)
