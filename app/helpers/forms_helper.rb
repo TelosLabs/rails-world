c52cd49bb34511c1d19e237e42f261761bb63973
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-07-23 14:44:49 -0600

    Update text fields placeholder opacity

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,3 +1,3 @@
 module FormsHelper
-  TEXT_FIELD_CLASSES = "w-full bg-transparent border border-2 border-white rounded-md text-white placeholder-white focus:bg-white focus:text-black focus:placeholder-gray focus:border-white focus:ring-white transition-all".freeze
+  TEXT_FIELD_CLASSES = "w-full bg-transparent border border-2 border-white rounded-md text-white placeholder-white/50 focus:bg-white focus:text-black focus:placeholder-gray focus:border-white focus:ring-white transition-all".freeze
 end
