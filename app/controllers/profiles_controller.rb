6ecec97a1a6463e264c6f0581902b836254233e2
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-07-24 19:29:23 -0600

    allow profile image to be deleted

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,8 +10,8 @@ class ProfilesController < ApplicationController
   def update
     @profile = current_profile
     @profile.assign_attributes(profile_params)
-
     if @profile.save
+      remove_profile_image_if_requested
       redirect_to profile_path, notice: t("controllers.profiles.update.success")
     else
       render :edit, status: :unprocessable_entity
@@ -30,5 +30,9 @@ class ProfilesController < ApplicationController
       :twitter_url, :linkedin_url, :github_url,
       :in_app_notifications, :mail_notifications
     )
+  end
+
+  def remove_profile_image_if_requested
+    @profile.image.purge if params[:profile][:remove_image].presence
   end
 end
--- app/javascript/controllers/image_previewer_controller.js
+++ app/javascript/controllers/image_previewer_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['image', 'input', 'hideOnLoad']
+  static targets = ['image', 
+                    'input', 
+                    'hideOnLoad', 
+                    'onRemoveImageTemplate', 
+                    'onRemoveImageContainer', 
+                    'onRemoveImagePlaceholderTemplate', 
+                    'onRemoveImagePlaceholderContainer']
 
   connect () {
     this.inputTarget.onchange = evt => {
@@ -10,11 +16,23 @@ export default class extends Controller {
         this.imageTarget.src = URL.createObjectURL(file)
         this.imageTarget.style.display = 'block'
         this.imageTarget.style.position = 'absolute'
-
+        this.onRemoveImageContainerTarget.innerHTML = ''
+        this.onRemoveImagePlaceholderContainerTarget.innerHTML = ''
         this.hideOnLoadTargets.forEach(element => {
           element.style.display = 'none'
         })
       }
     }
+  }
+
+  removeImage(event) {
+    this.imageTarget.src = ''
+    this.imageTarget.style.display = 'none'
+    this.inputTarget.value = ''
+    this.onRemoveImageContainerTarget.innerHTML = this.onRemoveImageTemplateTarget.innerHTML
+    this.onRemoveImagePlaceholderContainerTarget.innerHTML = this.onRemoveImagePlaceholderTemplateTarget.innerHTML
+    this.hideOnLoadTargets.forEach(element => {
+      element.style.display = 'none'
+    })
   }
 }
