04467cc062ae29c69699f957351ec8b47e9519bf
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-07-26 13:43:47 -0600

    update navbar

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,19 +1,11 @@
 module NavigationHelper
-  NAVIGATION_LINKS = {
-    agenda: "/",
-    schedule: "/",
-    profile: "/profile",
-    notifications: "/",
-    about: "/"
-  }
-
-  def nav_icon_class_for(item)
-    return "fill-red w-6 h-6" if current_page?(NAVIGATION_LINKS[item])
+  def nav_icon_class_for(path)
+    return "fill-red w-6 h-6" if path.any? { |p| current_page?(p) }
     "fill-slate-600 w-6 h-6"
   end
 
-  def nav_text_class_for(item)
-    return "text-red" if current_page?(NAVIGATION_LINKS[item])
+  def nav_text_class_for(path)
+    return "text-red" if path.any? { |p| current_page?(p) }
     "text-slate-600"
   end
 
