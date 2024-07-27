408470bf8cbee79f7a1f5b0a5e31d226815792ac
Alan Soto <soto.alan@hotmail.com>
2024-07-26 18:51:18 -0600

    Abstract reminder logic from Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -19,11 +19,7 @@
 #  index_events_on_location_id    (location_id)
 #
 class Event < ApplicationRecord
-  REMINDER_CADENCE = {
-    first_reminder: 15.minutes,
-    second_reminder: 10.minutes,
-    last_reminder: 0.minutes
-  }.freeze
+  include ReminderScheduler
 
   belongs_to :location
   belongs_to :conference
@@ -37,30 +33,4 @@ class Event < ApplicationRecord
   validates :ends_at, presence: true
 
   validates_datetime :ends_at, after: :starts_at
-
-  after_save_commit :schedule_reminder
-
-  private
-
-  # TODO: abtract reminder logic
-  def schedule_reminder
-    return unless saved_change_to_starts_at?
-
-    REMINDER_CADENCE.each do |reminder_type, cadence|
-      deadline = starts_at - cadence
-      next unless deadline > Time.zone.now
-
-      reminder_id = SecureRandom.uuid
-      reminder_details["#{reminder_type}_id"] = reminder_id
-      save
-
-      EventReminderJob
-        .set(wait_until: deadline)
-        .perform_later(
-          event_id: id,
-          reminder_type: reminder_type,
-          reminder_id: reminder_id
-        )
-    end
-  end
 end
