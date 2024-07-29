f275e131ae9339818a5af3ca55145c629f94940d
Andres Alvidrez <andres.ag4@hotmail.com>
2024-07-29 11:03:10 -0600

    remove 404 redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   include Authentication
-
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  private
-
-  def record_not_found
-    render file: Rails.public_path.join("404.html").to_s, status: :not_found, layout: false
-  end
 end
