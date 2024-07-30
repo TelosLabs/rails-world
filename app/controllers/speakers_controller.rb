c2a7c07cc4d65763e0eb364022e638df2ad9c2ec
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-07-30 14:32:59 -0600

    update speakers show

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -2,5 +2,6 @@ class SpeakersController < ApplicationController
   def show
     @speaker = Speaker.find(params[:id])
     @profile = @speaker.profile.presence || Profile.new
+    @events = @speaker.events
   end
 end
