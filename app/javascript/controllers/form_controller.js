4f6bd0f001a69fe254f9447366c3438ef121a46b
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-08-01 13:24:42 -0600

    linters changes

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,11 +2,11 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = ['form']
-  
+
   connect () {
   }
 
-  submit(event){
+  submit (event) {
     event.preventDefault()
     this.formTarget.requestSubmit()
   }
