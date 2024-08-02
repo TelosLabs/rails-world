20e0d781d61cd3cd3f8934dfb0cfe6bc2ddee35a
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-02 10:43:49 -0600

    Allow about page to be unauthenticated

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,14 @@ Rails.application.routes.draw do
   end
 
   constraints(AuthenticatedConstraint) do
-    resource :password, only: [:edit, :update]
-    resources :profiles, only: [:edit, :update], param: :uuid
+    root "main#index", as: :authenticated_root
   end
 
   resource :registration, only: [:new, :create]
   resource :user_session, only: [:new, :create, :destroy]
+  resource :password, only: [:edit, :update]
   resource :about, only: [:show]
-  resources :profiles, only: [:show], param: :uuid
+  resources :profiles, only: [:show, :edit, :update], param: :uuid
 
   resource :password_reset, only: [:new, :create, :edit, :update] do
     get :post_submit
