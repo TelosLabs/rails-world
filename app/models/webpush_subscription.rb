599479d8ed8823ed8512907bf3557bcbe98b24c6
Alan Soto <soto.alan@hotmail.com>
2024-08-02 09:38:10 -0600

    Add validation to webpush subscriptions model

--- app/models/webpush_subscription.rb
+++ app/models/webpush_subscription.rb
@@ -16,4 +16,8 @@
 #
 class WebpushSubscription < ApplicationRecord
   belongs_to :user
+
+  validates :auth, presence: true
+  validates :endpoint, presence: true
+  validates :p256dh, presence: true
 end
