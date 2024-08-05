c91cce5b789e03f783b9541af4c23d9e6e90fb3b
Alan Soto <soto.alan@hotmail.com>
2024-08-05 16:41:47 -0600

    Remove service-worker

---
+++ public/service_worker.js
@@ -0,0 +1,14 @@
+/* global self */
+
+self.addEventListener('push', (event) => {
+  const data = event.data.json()
+  const title = data.title
+  const options = {
+    body: data.body,
+    icon: data.icon
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  )
+})
