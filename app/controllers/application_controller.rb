7e98059d7ce28d9260a4328780967a40dafd57bd
Alan Soto <soto.alan@hotmail.com>
2024-08-05 18:00:49 -0600

    Moving vapid key to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,14 @@ class ApplicationController < ActionController::Base
   default_form_builder ApplicationFormBuilder
 
   helper_method :current_profile
+  helper_method :vapid_public_key
 
   private
 
   def current_profile = current_user&.profile
+
+  def vapid_public_key
+    vapid_key = Rails.application.credentials.dig(:vapid, :public_key)
+    Base64.urlsafe_decode64(vapid_key).bytes
+  end
 end
