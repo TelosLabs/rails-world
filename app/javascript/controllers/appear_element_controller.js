f19406bb5a34f2e0ec9c02321ab6a33d68228e72
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-08-06 10:35:57 -0600

    linters

--- app/javascript/controllers/appear_element_controller.js
+++ app/javascript/controllers/appear_element_controller.js
@@ -1,8 +1,8 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  initialize(){
-    this.element.classList.add("opacity-0", "transition", "ease-in-out")
+  initialize () {
+    this.element.classList.add('opacity-0', 'transition', 'ease-in-out')
   }
 
   connect () {
@@ -11,8 +11,8 @@ export default class extends Controller {
 
   show (event) {
     setTimeout(() => {
-      this.element.classList.remove("opacity-0")
-      this.element.classList.add("opacity-1")
+      this.element.classList.remove('opacity-0')
+      this.element.classList.add('opacity-1')
     }, 600)
   }
 }
