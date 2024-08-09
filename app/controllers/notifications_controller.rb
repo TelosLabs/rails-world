1638cd2cdc7160268c162b8ac770cad2c9764fb8
Alan Soto <soto.alan@hotmail.com>
2024-08-09 15:25:00 -0600

    Notifications empty state

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,8 @@
 class NotificationsController < ApplicationController
   def index
     threshold = 10.minutes.ago
-    notifications = current_user.notifications.unread.order(created_at: :desc)
-    @recent_notifications = notifications.where(created_at: threshold..)
-    @past_notifications = notifications.where(created_at: ...threshold)
+    @notifications = current_user.notifications.unread.order(created_at: :desc)
+    @recent_notifications = @notifications.where(created_at: threshold..).limit(20)
+    @past_notifications = @notifications.where(created_at: ...threshold).limit(10)
   end
 end
