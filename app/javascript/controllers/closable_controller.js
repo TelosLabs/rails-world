80ee8ecaed2f34961dbd16ca4497f6506f0fe551
Edgar Bustillos <edgar_daniel_1996@hotmail.com>
2024-08-12 13:16:03 -0600

    refactor flash message

--- app/javascript/controllers/closable_controller.js
+++ app/javascript/controllers/closable_controller.js
@@ -1,19 +1,32 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  initialize () {
+    this._closeTimeout = null
+  }
+  
   connect () {
     this.close()
   }
 
+  cancelClose () {
+    if(this._closeTimeout){
+      clearTimeout(this._closeTimeout)
+      this._closeTimeout = null
+    }
+  }
+
   close (event) {
-    setTimeout(() => {
-      // Animate element to disappear towards the right
-      this.element.style.transition = 'transform 600ms, opacity 600ms'
-      this.element.style.transform = 'translate(200px, 0px)'
-      this.element.style.opacity = 0
-      setTimeout(() => {
-        this.element.remove()
-      }, 600)
-    }, 3000)
+    if(this._closeTimeout == null){
+      this._closeTimeout = setTimeout(() => {
+        // Animate element to disappear towards the right
+        this.element.style.transition = 'transform 600ms, opacity 600ms'
+        this.element.style.transform = 'translate(200px, 0px)'
+        this.element.style.opacity = 0
+        setTimeout(() => {
+          this.element.remove()
+        }, 600)
+      }, 3000)
+    }
   }
 }
