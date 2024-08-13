09b41d876c628580460df5fe95c73dd2002ceab2
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-13 11:21:10 -0600

    Fix SVG coloring and checkbox label

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     end
   end
 
-  def show_filters_pills?
+  def active_filters?
     SessionQuery::STATUS_SCOPES.any? { |status| request.params[status] == "1" }
   end
 end
