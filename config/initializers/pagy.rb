8c2aee9a785be905c3cc291b940f6cd8e41ba9f2
Alan Soto <soto.alan@hotmail.com>
2024-08-13 12:34:14 -0600

    Fix linterns

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -8,7 +8,7 @@
 # See https://ddnexus.github.io/pagy/docs/api/pagy#variables
 # You can set any pagy variable as a Pagy::DEFAULT. They can also be overridden per instance by just passing them to
 # Pagy.new|Pagy::Countless.new|Pagy::Calendar::*.new or any of the #pagy* controller methods
-# Here are the few that make more sense as DEFAULTs:
+# Here are the few that make more sense as DEFAULTTs:
 # Pagy::DEFAULT[:limit]       = 20                    # default
 Pagy::DEFAULT[:size] = [1, 1, 1, 1]
 # Pagy::DEFAULT[:ends]        = true                  # default
