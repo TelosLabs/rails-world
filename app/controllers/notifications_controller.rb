dec649861a1bf83b9d03eb5c86f3cc3ccd68e38c
Alan Soto <soto.alan@hotmail.com>
2024-08-13 10:28:58 -0600

    Remove recent/past

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,5 @@
 class NotificationsController < ApplicationController
   def index
-    threshold = 10.minutes.ago
     @notifications = current_user.notifications.unread.order(created_at: :desc)
-    @recent_notifications = @notifications.where(created_at: threshold..).limit(20)
-    @past_notifications = @notifications.where(created_at: ...threshold).limit(10)
   end
 end
