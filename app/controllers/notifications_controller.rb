7fad1979e6446cb0c65684f86b14ed95a88b6f10
Sergio-e <33036058+Sergio-e@users.noreply.github.com>
2024-08-14 20:55:05 -0600

    Syntax improvements

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,9 @@ class NotificationsController < ApplicationController
   include Pagy::Backend
 
   def index
-    notifications = current_user.notifications.unread.includes(event: {record: :speakers}).order(created_at: :desc)
+    notifications = current_user.notifications.unread
+      .includes(event: {record: :speakers})
+      .order(created_at: :desc)
     @pagy, @notifications = pagy notifications, items: 5
   end
 end
