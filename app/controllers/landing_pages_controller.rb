0b1c09494508eecdecea1949517ed4264a4b1526
Alan Soto <soto.alan@hotmail.com>
2024-08-15 12:05:58 -0600

    Redirect traffic to landing page

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,14 +1,6 @@
 class LandingPagesController < ApplicationController
   allow_unauthenticated_access only: [:show]
 
-  helper_method :show_header?
-
   def show
-  end
-
-  private
-
-  def show_header?
-    false
   end
 end
