6e6c5b642c3e9619a3459e4953ab52a8efe91bd1
Sergio-e <33036058+Sergio-e@users.noreply.github.com>
2024-08-14 20:29:29 -0600

    Improvements

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,9 @@
 class SchedulesController < ApplicationController
   def show
-    @sessions = current_user.sessions.includes(:attendees, :speakers, :location, :tags)
+    @sessions = current_user.sessions
+      .where(conference: current_conference)
+      .includes(:attendees, :speakers, :location, :tags)
+
     @sessions = @sessions.starts_at(params[:starts_at].to_date) if params[:starts_at].present?
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def index
-    @sessions = sessions.joins(:speakers, :location).includes(:attendees, :tags)
+    @user_session_ids = current_user.sessions.pluck(:id)
+    @sessions = sessions
+      .joins(:speakers, :location)
+      .includes(:attendees, :tags)
+
     @sessions = @sessions.starts_at(params[:starts_at].to_date) if params[:starts_at].present?
   end
 
