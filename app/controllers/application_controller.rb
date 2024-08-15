5d29cfc2e25e1c6f38cf142625c28cf39dba6623
Sergio-e <33036058+Sergio-e@users.noreply.github.com>
2024-08-15 16:59:24 -0600

    Better coming soon page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
+  include Onboarding
   include Authentication
   include Authorization
 
   default_form_builder ApplicationFormBuilder
 
   helper_method :current_profile, :current_conference, :vapid_public_key
-
-  before_action :redirect_to_landing_page
 
   private
 
@@ -17,11 +16,5 @@ class ApplicationController < ActionController::Base
 
   def vapid_public_key
     Base64.urlsafe_decode64(ENV["VAPID_PUBLIC_KEY"]).bytes.to_json
-  end
-
-  def redirect_to_landing_page
-    if Feature.enabled?(:landing_page) && request.path != landing_page_path
-      redirect_to landing_page_path
-    end
   end
 end
