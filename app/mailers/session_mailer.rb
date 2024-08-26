c659492d0510fbedc76e706acd31d3d308f19ec3
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-26 14:45:49 -0600

    Fix Session Reminder subject and icons path

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -4,7 +4,17 @@ class SessionMailer < ApplicationMailer
     @recipient = params[:recipient]
     @session = params[:record]
     @speakers = @session.speakers
+    @notification = params[:notification]
+    @subject = reminder_subject
 
-    mail(to: @recipient.email, subject: t("mailers.session_mailer.reminder.subject"))
+    mail(to: @recipient.email, subject: @subject)
+  end
+
+  private
+
+  def reminder_subject
+    return t("mailers.session_mailer.reminder.subject.without_time") unless @notification&.params&.dig(:time_before_session)
+
+    t("mailers.session_mailer.reminder.subject.with_time", time_before_session: @notification&.params&.dig(:time_before_session))
   end
 end
