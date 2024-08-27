b963c4f959707420326afbdbf11aaf616f593f5f
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-27 12:31:13 -0600

    Fix bookmark turbo action

--- app/javascript/controllers/preserve_scroll_controller.js
+++ app/javascript/controllers/preserve_scroll_controller.js
@@ -8,31 +8,17 @@ export default class extends Controller {
 
     window.addEventListener('turbo:before-render', this.preserveScroll)
     window.addEventListener('turbo:render', this.restoreScroll)
-    window.addEventListener('turbo:load', this.restoreBodyScroll)
   }
 
   preserveScroll () {
     document.querySelectorAll('[data-preserve-scroll]').forEach((element) => {
       window.scrollPositions[element.id] = element.scrollLeft
     })
-
-    // Preserve scroll position of body
-    window.scrollPositions.body = {
-      scrollTop: window.scrollY
-    }
   }
 
   restoreScroll () {
     document.querySelectorAll('[data-preserve-scroll]').forEach((element) => {
       element.scrollLeft = window.scrollPositions[element.id]
     })
-  }
-
-  restoreBodyScroll () {
-    const bodyPositions = window.scrollPositions.body
-
-    if (bodyPositions) {
-      window.scrollTo(0, bodyPositions.scrollTop)
-    }
   }
 }
