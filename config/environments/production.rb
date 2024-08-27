b83ade835679a8e329feaa7e4a131e0c03374f32
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-27 10:27:53 -0600

    Fix email footer icon and asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailpace
   config.action_mailer.mailpace_settings = {api_token: ENV["MAILPACE_API_TOKEN"]}
   config.action_mailer.default_url_options = {host: ENV["APP_HOST"]}
+  config.action_mailer.asset_host = ENV["APP_HOST"]
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
