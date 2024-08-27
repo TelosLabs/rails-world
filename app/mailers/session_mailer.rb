fa9c0c69cfbd30b99ea938ff151dd6c395bab934
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-26 18:41:39 -0600

    Fix

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -1,10 +1,10 @@
 class SessionMailer < ApplicationMailer
   helper :datetime
+
   def reminder
     @recipient = params[:recipient]
     @session = params[:record]
     @speakers = @session.speakers
-    @notification = params[:notification]
     @subject = reminder_subject
 
     mail(to: @recipient.email, subject: @subject)
@@ -13,8 +13,8 @@ class SessionMailer < ApplicationMailer
   private
 
   def reminder_subject
-    return t("mailers.session_mailer.reminder.subject.without_time") unless @notification&.params&.dig(:time_before_session)
+    return t("mailers.session_mailer.reminder.subject.without_time") if params[:time_before_session].blank?
 
-    t("mailers.session_mailer.reminder.subject.with_time", time_before_session: @notification&.params&.dig(:time_before_session))
+    t("mailers.session_mailer.reminder.subject.with_time", time_before_session: params[:time_before_session])
   end
 end
