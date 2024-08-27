b5a0c310b7c53ae6065c406712cd4b8edbd1a7ec
Andres Alvidrez <andres.ag4@hotmail.com>
2024-08-27 09:51:18 -0600

    refactor

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -5,16 +5,8 @@ class SessionMailer < ApplicationMailer
     @recipient = params[:recipient]
     @session = params[:record]
     @speakers = @session.speakers
-    @subject = reminder_subject
+    @notification = params[:notification]
 
-    mail(to: @recipient.email, subject: @subject)
-  end
-
-  private
-
-  def reminder_subject
-    return t("mailers.session_mailer.reminder.subject.without_time") if params[:time_before_session].blank?
-
-    t("mailers.session_mailer.reminder.subject.with_time", time_before_session: params[:time_before_session])
+    mail(to: @recipient.email, subject: @notification.subject)
   end
 end
