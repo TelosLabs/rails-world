112902f372df81b7ee431cdc676dd1773f4d30cd
Alan Soto <soto.alan@hotmail.com>
2024-08-30 12:52:33 -0600

    fix linterns

--- app/javascript/controllers/pwa_install_prompt_controller.js
+++ app/javascript/controllers/pwa_install_prompt_controller.js
@@ -1,41 +1,41 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["installBtn"];
+  static targets = ['installBtn']
 
   connect () {
-    this.installPrompt = null;
+    this.installPrompt = null
 
     if (this.hasInstallBtnTarget) {
-      this.setupInstallPrompt();
-      this.setupAppInstalledListener();
+      this.setupInstallPrompt()
+      this.setupAppInstalledListener()
     }
   }
 
-  setupInstallPrompt() {
-    window.addEventListener("beforeinstallprompt", (event) => {
-      event.preventDefault();
+  setupInstallPrompt () {
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault()
 
-      this.installPrompt = event;
-      this.installBtnTarget.removeAttribute("hidden");
-    });
+      this.installPrompt = event
+      this.installBtnTarget.removeAttribute('hidden')
+    })
 
-    this.installBtnTarget.addEventListener("click", async () => {
+    this.installBtnTarget.addEventListener('click', async () => {
       if (!this.installPrompt) { return }
 
-      await this.installPrompt.prompt();
-      this.disableInAppInstallPrompt();
-    });
+      await this.installPrompt.prompt()
+      this.disableInAppInstallPrompt()
+    })
   }
 
-  setupAppInstalledListener() {
-    window.addEventListener("appinstalled", () => {
-      this.disableInAppInstallPrompt();
-    });
+  setupAppInstalledListener () {
+    window.addEventListener('appinstalled', () => {
+      this.disableInAppInstallPrompt()
+    })
   }
 
-  disableInAppInstallPrompt() {
-    this.installPrompt = null;
-    this.installBtnTarget.setAttribute("hidden", "");
+  disableInAppInstallPrompt () {
+    this.installPrompt = null
+    this.installBtnTarget.setAttribute('hidden', '')
   }
 }
