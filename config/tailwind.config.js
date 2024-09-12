98fc6001d92548a6d2cb39458453bf94856eb4da
Andres Alvidrez <andres.ag4@hotmail.com>
2024-09-11 18:38:14 -0600

    Allow easy access to customize styling colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,6 +20,31 @@ module.exports = {
         'screen-sm': '425px'
       },
       colors: {
+        primary: {
+          50: '#edeaf1',
+          200: '#c8bdd4',
+          100: '#ae9dbf',
+          300: '#8870a1',
+          400: '#71558f',
+          500: '#4e2a73',
+          600: '#432463',
+          700: '#371e52',
+          800: '#2b173f',
+          900: '#211230',
+          background: '#4e2a73'
+        },
+        secondary: {
+          50: '#FAE7E8',
+          100: '#EFB4B9',
+          200: '#E78F97',
+          300: '#DC5C67',
+          400: '#D53D49',
+          500: '#CB0C1C',
+          600: '#B90B19',
+          700: '#900914',
+          800: '#70070F',
+          900: '#55050C'
+        },
         grey: {
           50: '#F3F2F8',
           200: '#D7D7D8',
@@ -37,9 +62,8 @@ module.exports = {
           200: '#CDD9EC',
           600: '#829ECE'
         },
-        'purple-dark': '#432463',
-        'purple-light': '#4E2A73',
-        red: '#CB0C1C',
+
+        red: '#CB0C1C', // same as secondary 500
         'blue-marine': '#003A5D',
         'blue-teal': '#00AFAA',
         yellow: '#F4BF4F'
