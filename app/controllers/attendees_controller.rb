36eb3bf2b48669f9a50ec6bbc703ed394383e1e3
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-09-19 08:52:20 +0300

    create render notice partial view helper

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,9 +1,12 @@
 class AttendeesController < ApplicationController
-  before_action :set_session, only: [:create, :destroy]
+  before_action :set_session
 
   def create
     @session.attendees.push(current_user)
     flash[:notice] = I18n.t("controllers.attendees.add_user.notice")
+
+    session[:last_action] = {action: "create", user_id: current_user.id, session_id: @session.id}
+    flash[:undo_path] = undo_session_attendee_path(session_id: @session.id)
 
     redirect_back_or_to(sessions_path, params: params[:starts_at])
   end
@@ -12,6 +15,14 @@ class AttendeesController < ApplicationController
     @session.attendees.delete(current_user)
     flash[:notice] = I18n.t("controllers.attendees.remove_user.notice")
 
+    session[:last_action] = {action: "destroy", user_id: current_user.id, session_id: @session.id}
+    flash[:undo_path] = undo_session_attendee_path(session_id: @session.id)
+
+    redirect_back_or_to(sessions_path, params: params[:starts_at])
+  end
+
+  def undo
+    binding.pry
     redirect_back_or_to(sessions_path, params: params[:starts_at])
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def render_notice_flash
+    notice_message = flash[:notice]
+    undo_path = flash[:undo_path]
+
+    return if notice_message.blank?
+
+    render partial: "layouts/flash_message", locals: {message: notice_message, undo_path: undo_path}
+  end
 end
