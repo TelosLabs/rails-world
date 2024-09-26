6be14431d7a4cfbd794296edf4b14cccfc189821
Sergio-e <33036058+Sergio-e@users.noreply.github.com>
2024-09-25 23:37:46 -0400

    Bug fix and improvements

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,16 @@
 class NotificationsController < ApplicationController
   include Pagy::Backend
 
+  after_action :mark_as_read
+
   def index
-    notifications = current_user.notifications.unread.newest_first
+    notifications = current_user.notifications.newest_first
     @pagy, @notifications = pagy notifications, items: 5
+  end
+
+  private
+
+  def mark_as_read
+    current_user.notifications.unread.mark_as_read
   end
 end
