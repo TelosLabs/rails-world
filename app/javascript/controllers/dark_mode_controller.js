5ad9e5231926b06176a727b1ea0275ce183b1e54
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-08-07 15:37:09 -0600

    fix linter warnings

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -1,28 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["switch"]
+  static targets = ['switch']
 
   connect() {
-    const isDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    const isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
 
-    document.documentElement.classList.toggle("dark", isDark)
+    document.documentElement.classList.toggle('dark', isDark)
     this.updateSwitch(isDark)
   }
 
   toggle() {
-    const enableDark = !document.documentElement.classList.contains("dark")
-    document.documentElement.classList.toggle("dark", enableDark)
-    localStorage.theme = enableDark ? "dark" : "light"
+    const enableDark = !document.documentElement.classList.contains('dark')
+    document.documentElement.classList.toggle('dark', enableDark)
+    localStorage.theme = enableDark ? 'dark' : 'light'
     this.updateSwitch(enableDark)
   }
 
   updateSwitch(isDark) {
-    const ball = this.switchTarget.querySelector("span:last-child")
+    const ball = this.switchTarget.querySelector('span:last-child')
     if (isDark) {
-      ball.classList.add("dark:translate-x-6")
+      ball.classList.add('dark:translate-x-6')
     } else {
-      ball.classList.remove("dark:translate-x-6")
+      ball.classList.remove('dark:translate-x-6')
     }
   }
 }
