a3d1bc3dd5c472361bb69b937700c68fe98f8495
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-08-07 15:38:42 -0600

    fix function lint warning

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -3,21 +3,21 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['switch']
 
-  connect() {
+  connect () {
     const isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
 
     document.documentElement.classList.toggle('dark', isDark)
     this.updateSwitch(isDark)
   }
 
-  toggle() {
+  toggle () {
     const enableDark = !document.documentElement.classList.contains('dark')
     document.documentElement.classList.toggle('dark', enableDark)
     localStorage.theme = enableDark ? 'dark' : 'light'
     this.updateSwitch(enableDark)
   }
 
-  updateSwitch(isDark) {
+  updateSwitch (isDark) {
     const ball = this.switchTarget.querySelector('span:last-child')
     if (isDark) {
       ball.classList.add('dark:translate-x-6')
