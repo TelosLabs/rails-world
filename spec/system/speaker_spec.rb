3942d896bbffe2ce1d74b085ce2bdebecff2fd3f
yash-learner <yash.topaz@gmail.com>
2025-08-17 18:08:37 +0530

    Add spec for speaker
    - check for presence of links when profile has
    - check for not presence of links when  profile does not

---
+++ spec/system/speaker_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "Speaker", type: :system do
+  let!(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+  end
+
+  context "when speaker has social links" do
+    let!(:session) { create(:session, :with_speakers) }
+    let!(:speaker) { session.speakers.first }
+
+    it "displays social links" do
+      visit speaker_path(speaker)
+
+      expect(page).to have_link(href: "https://github.com")
+      expect(page).to have_link(href: "https://twitter.com")
+      expect(page).to have_link(href: "https://linkedin.com")
+    end
+  end
+
+  context "when speaker does not have social links" do
+    let!(:session) { create(:session, :with_speakers) }
+    let!(:speaker) { session.speakers.first }
+
+    before do
+      speaker.profile.update(github_url: nil, twitter_url: nil, linkedin_url: nil)
+    end
+
+    it "does not display social links" do
+      visit speaker_path(speaker)
+
+      expect(page).not_to have_link(href: "https://github.com")
+      expect(page).not_to have_link(href: "https://twitter.com")
+      expect(page).not_to have_link(href: "https://linkedin.com")
+    end
+  end
+end
