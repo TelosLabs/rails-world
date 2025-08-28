d4da65a936c41a48a60e1ff7905c007b6eff480d
LuigiR0jas <luigirojasdev@gmail.com>
2025-08-27 18:51:58 -0600

    minor improvements

--- app/builders/application_form_builder.rb
+++ app/builders/application_form_builder.rb
@@ -4,7 +4,7 @@ class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
   STYLES = {
     default: "bg-transparent border border-2 border-white rounded-md text-white
     placeholder-white/50 focus:bg-white focus:text-black focus:placeholder-grey-300
-    focus:border-white focus:ring-white transition-all",
+    focus:border-white focus:ring-white transition-all dark:border-gray-600 dark:text-white focus:dark:bg-transparent dark:focus:border-gray-600",
     toggle_field: "relative w-11 h-6 bg-grey-300 rounded-full peer dark:bg-grey-300
     peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
     peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px]
